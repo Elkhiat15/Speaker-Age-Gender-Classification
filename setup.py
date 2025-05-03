90d6836dfeac26a34e5a059cc3b5cbb6e624ed20
Mohammed Elkhiat <89255892+Elkhiat15@users.noreply.github.com>
2025-05-03 18:01:25 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='project_linker',
+    version='0.1',
+    packages=find_packages()
+)
+
+### This setup script is used to package the `packages_linker` module.
